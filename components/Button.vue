b6d0ed9c2d4c799e31efc04ae907a1f4b987766a
Laurenz Guevara <laurenz@theunloved.co.uk>
2023-10-26 17:07:59 +0100

    Added reusable button example

---
+++ components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    :class="[
+    className ? className : 'p-4',
+    {
+    primary: 'bg-white text-black',
+    secondary: 'bg-green-500 text-red',
+    }[type],
+    disabled ? 'opacity-50' : 'opacity-100',
+    'transition-all'
+    ]"
+    :disabled="disabled"
+    @click="onClick"
+    v-bind="$attrs"
+    >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      type: {
+        type: String,
+        default: 'primary'
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      onClick: Function,
+      className: String
+    }
+  };
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
-    <section class="bg-black">
-      <h1>Hello World</h1>
+    <section class="bg-black w-screen h-screen grid place-items-center">
+      <div class="grid gap-4">
+        <Button
+          :isLoading="isLoading"
+          :type="'primary'"
+          class="py-0"
+          >
+          Click Me
+        </Button>
+          <Button
+            :isLoading="isLoading"
+            :type="'secondary'"
+            :disabled="false"
+            class="py-0"
+            @click="console.log('I pressed a button!')"
+            >
+            Click Me
+          </Button>
+      </div>
     </section>
   </div>
 </template>
+
+<script>
+  import Button from '../components/Button.vue'; 
+
+  export default {
+    name: 'Index',
+    components: {
+      Button
+    },
+    data() {
+      return {
+        isLoading: false, 
+      };
+    }
+  };
+</script>
 
